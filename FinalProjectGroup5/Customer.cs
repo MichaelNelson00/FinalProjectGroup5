ea7fee5c4e971658eb5d9972a5ec413659b7b32d
ronhegde <152325953+ronhegde@users.noreply.github.com>
2023-12-08 14:45:38 -0600

    Update Customer.cs

--- FinalProjectGroup5/Customer.cs
+++ FinalProjectGroup5/Customer.cs
@@ -3,19 +3,51 @@ namespace FinalProjectGroup5;
 
 public class Customer
 {
-	public int customerID;
-	public string fullName;
-	public string address;
-	public int phoneNumber;
-	public string dateofBirth;
+    private int customerID;
+    private string fullName;
+    private string address;
+    private int phoneNumber;
+    private string dateofBirth;
+    private List<serialNumber> ReservedItems = new List<serialNumber>;
 
-	public Customer(int customerid, string fullname, string Address, int phonenumber, string dateofbirth)
-	{
-		customerID = customerid;
-		fullName = fullname;
-		address = Address;
-		phoneNumber = phonenumber;
-		dateofBirth = dateofbirth;
-	}
+    public Customer(int customerid, string fullname, string Address, int phonenumber, string dateofbirth)
+    {
+        this.customerID = customerid;
+        this.fullName = fullname;
+        this.address = Address;
+        this.phoneNumber = phonenumber;
+        this.dateofBirth = dateofbirth;
+    }
+
+    public List<serialNumber> ItemsReserved()
+    {
+        return ReservedItems;
+    }
+
+    public decimal OverdueFines()
+    {
+
+    }
+
+    public void Reserve(serialNumber item, DateTime startDate, int durationHours)
+    {
+
+    }
+
+    public void Request(serialNumber item)
+    {
+
+    }
+
+    public DateTime WhenDue(serialNumber item)
+    {
+
+    }
+
+    public decimal DamageFees(serialNumber item)
+    {
+
+    }
+
 }
 
